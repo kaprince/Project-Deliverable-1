901d2c078722a794ea0b8d52ade2b6c865768ee5
kaprince <kaprince@sheridancollege.ca>
2019-06-12 10:26:13 -0400

    Project-1

---
+++ src/ca/sheridancollege/project/Player.java
@@ -0,0 +1,50 @@
+/**
+ * SYST 17796 Project Summer 2019 Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as a modifier and the date!
+ * @modifier (Group-7) Prince Preet Kaur, Mehak Mehak,Jasleen Kaur Chahal
+ * Date: June 12, 2019
+ */
+package ca.sheridancollege.project;
+
+/**
+ * A class that models each Player in the game. Players have an identifier, which should be unique.
+ * 
+ */
+public abstract class Player 
+{
+    private String playerID; //the unique ID for this player
+    
+    /**
+     * A constructor that allows you to set the player's unique ID
+     * @param name the unique ID to assign to this player.
+     */
+    public Player(String name)
+    {
+        playerID= name;
+    }
+    
+    /**
+     * @return the playerID
+     */
+    public String getPlayerID() 
+    {
+        return playerID;
+    }
+
+    /**
+     * Ensure that the playerID is unique
+     * @param givenID the playerID to set
+     */
+    public void setPlayerID(String givenID) 
+    {
+        playerID = givenID;
+    }
+    
+    /**
+     * The method to be instantiated when you subclass the Player class
+     * with your specific type of Player and filled in with logic to play your game.
+     */
+    public abstract void play();
+    
+}
